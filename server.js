10544722396240e9a28e7f833305119e55c2ff2d
Mike <merklerner@gmail.com>
2020-10-13 22:54:37 -0700

    cleans up home route

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ const PORT = 3000
 
 app.get('/', function(req, res){
 	let filepath = path.join(__dirname, 'home.html')
-
-	console.log("*------------------------*")
-	console.log("this is __dirname", __dirname)
-	console.log(filepath)
-	console.log("*------------------------*")
-
-
-
     res.sendFile(filepath)
 })
 
