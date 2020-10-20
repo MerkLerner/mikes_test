6195f14b1994c272e8a856e3265d9f1deda49217
Mike <merklerner@gmail.com>
2020-10-19 21:21:00 -0700

    ill advised commit

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const path = require('path')
 const app = express()
 const PORT = 3000
 
+app.set('view engine', 'ejs')
+
+
+let data = { viewCount: 0 }
+
+
 app.get('/', function(req, res){
-	let filepath = path.join(__dirname, 'home.html')
-    res.sendFile(filepath)
+ 
+ data.viewCount += 1
+ res.render('home', {count:  data.viewCount  }   )
+
 })
 
 app.get('/favorites', function(req,res){
